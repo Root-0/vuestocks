0de0e7967e5daac7f6ac2290f58aaff36ccc98c9
Akshay <akshayrajput102@gmail.com>
2020-10-23 18:17:46 +0530

    Add fontawesome, created header component

---
+++ src/components/siteHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="siteHeader">
+    <!-- test tailwindcss -->
+    <div class="container mx-auto">
+      <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+        <p class="font-bold"><span class="fab fa-dashcube"></span>
+         Be Warned</p>
+        <p>Something not ideal might be happening.</p>
+      </div>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Stonks',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import './assets/fontawesome5/css/all.min.css'
 
 Vue.config.productionTip = false
 
